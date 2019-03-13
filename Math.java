b8e054cef06a1f07ba51f880d824c04940c79f74
Ahtshamulhassan <48089641+Ahtshamulhassan@users.noreply.github.com>
2019-03-13 23:37:03 +0500

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,10 @@
+class Math{
+public static void main(String args[]){
+int a=6+16/4-3;
+int b=2+2*4/5+1;
+int c=2+2*(2*2-2)%2/2;
+System.out.println("result:" +a);
+System.out.println("result:" +b);
+System.out.println("result:" +c);
+}
+}
