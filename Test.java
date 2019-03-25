c1e8ee4456fb3ef6fa2282603c2685ccb7dbd53c
Ahtshamulhassan <48089641+Ahtshamulhassan@users.noreply.github.com>
2019-03-26 00:13:37 +0500

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,7 +1,19 @@
-class Test{
-public static void main(String args[])
+import java.util.Scanner;
+class Test
 {
-System.out.println("Good Boy");
-System.out.print("Bad Boy");
+public static void main (String args[])
+{
+Scanner obj=new Scanner(System.in);
+int sal ,b;
+System.out.println("Enter your sallery");
+sal=obj.nextInt();
+if (sal>=10000)
+{
+b=(sal*10)/100;
+sal=sal+b;
+}
+System.out.println("Sallery is" +sal);
 }
 }
+
+
