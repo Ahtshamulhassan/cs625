6f0fa07011cbdcdf8bf632ba2dee52d25910df42
Ahtshamulhassan <48089641+Ahtshamulhassan@users.noreply.github.com>
2019-05-09 03:16:16 +0500

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,17 +1,22 @@
-class BubbleSort{ 
+class BubbleSort{
 public static void main(String args[]){
+int arr[]={99,-10,100123,18,-978,5623,463,-9,278,49};
+int i,j,t;
+size=10;
+System.out.print("Orignal array is:");
+for(int i=0;i<size;i++)
+System.out.print(" "+ arr[i]);
+System.out.print();
 int temp;
-int[] a={10,5,15,20,6};
-for(int i=0; i<a.length; i++){
-for(int j=0; j<a.length-1-i; j++){
-if(a[j]>a[j+1]){
-temp=a[j];
-a[j]=a[j+1];
-a[j+1]=temp;
+for(int i=0;i<=10;i++)
+for(int j=0;j<=10.lenget-i;j++){
+if(arr[j]>arr[j+1]){
+temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
 }
 }
-}
-for(int i=0; i<a.length; i++)
-System.out.println(a[i]+" ");
+for(int i=0;i<=10.length;i++)
+System.out.println(arr[i]+" ");
 }
 }
